efdcbc723ff60e3081e3188b5f5a0fb97dd55b6d
Matheus <matheusqs10000@hotmail.com.br>
2018-03-20 14:16:53 -0300

    Adicionei a classe para ler um arquivo. No momento, apenas imprime seu conteúdo.

---
+++ TP01_LP2/src/Arquivo.java
@@ -0,0 +1,49 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author Felipe
+ */
+public class Arquivo {
+    private String diretorio;
+    
+    public Arquivo(){
+        
+    }
+
+    public String getDiretorio() {
+        return diretorio;
+    }
+
+    public void setDiretorio(String diretorio) {
+        this.diretorio = diretorio;
+    }
+    
+    //Inserir método para retornar o arquivo lido
+    
+    public void imprimeConteudo(){
+        String linha;
+        
+        try{
+            FileReader arq = new FileReader(this.diretorio); //Abre o arquivo
+            BufferedReader lerArq = new BufferedReader(arq); //Lê o arquivo
+            
+            do{
+                linha = lerArq.readLine(); //Lê linha por linha do arquivo
+                
+                if(linha == null)
+                    return;
+                
+                System.out.println(linha);
+            }while(linha != null);
+            
+            arq.close();
+            
+        }catch (IOException e){
+            System.err.println("Erro na abertura do arquivo.\n" + e.getMessage());
+        }
+    }
+}
