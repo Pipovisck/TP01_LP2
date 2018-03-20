1639af625bc61a200768732f61d9000b2b5402e2
Matheus Quintão Santiago <matheusqs10000@hotmail.com.br>
2018-03-19 22:53:01 -0300

    Criei a classe para servir de 'memoria' para as variaveis.

---
+++ TP01_LP2/src/Memoria.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author mathe
+ * @param <E>
+ */
+public class Memoria<E> {
+
+    private HashMap<String, E> variaveis;
+
+    public Memoria() {
+        variaveis = new HashMap<>();
+    }
+
+    public void add(String nomeVariavel, E valorVariavel) {
+        this.variaveis.put(nomeVariavel, valorVariavel);
+    }
+
+    public void remove(String nomeVariavel) {
+        this.variaveis.remove(nomeVariavel);
+    }
+
+    public E getVariavel(String nomeVariavel) {
+        return variaveis.get(nomeVariavel);
+    }
+    
+    public void setVariavel(String nomeVariavel, E valorVariavel){
+        variaveis.replace(nomeVariavel, valorVariavel);
+    }
+}
