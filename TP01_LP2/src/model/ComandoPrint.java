200194868669003f019a32e67ad35b762754500d
JamalzGemaplys <gabriel44558888@gmail.com>
2018-04-07 22:33:25 -0300

    Adicionei o comando println

--- TP01_LP2/src/model/ComandoPrint.java
+++ TP01_LP2/src/model/ComandoPrint.java
@@ -7,12 +7,28 @@ public class ComandoPrint extends Comando<String>{
     private String linhaComando;
     char[] vetorComando;
     String impressao;
+    int inicioParametros;
 
     public ComandoPrint(String linhaComando){
         super();
         this.linhaComando = linhaComando;
         vetorComando = linhaComando.toCharArray();
         impressao = new String();
+        
+        if(vetorComando[5]=='l'){
+            inicioParametros=8;
+        }
+        else if(vetorComando[5]==' '){
+            for(int i=5; i<vetorComando.length; i++){
+                if(vetorComando[i]=='('){
+                    inicioParametros = i+1;
+                    break;
+                }
+            }
+        }
+        else{
+            inicioParametros=6;
+        }
     }
 
     @Override
@@ -24,8 +40,8 @@ public class ComandoPrint extends Comando<String>{
         boolean ehString = false;
         String nomeVariavel = new String();
 
-        for(int i=6; i<vetorComando.length; i++){
-            if(i==6 && vetorComando[i]!='"'){
+        for(int i=inicioParametros; i<vetorComando.length; i++){
+            if(i==inicioParametros && vetorComando[i]!='"'){
                 concatenaVariavel=true;
                 contaVariaveis++;
             }
@@ -48,7 +64,7 @@ public class ComandoPrint extends Comando<String>{
 
         contaStrings=contaStrings/2;
 
-        for (int i = 6; i < vetorComando.length; i++) {
+        for (int i = inicioParametros; i < vetorComando.length; i++) {
             if(vetorComando[i]=='"'&&ehString==false){
                 ehString = true;
             }
@@ -58,7 +74,7 @@ public class ComandoPrint extends Comando<String>{
             else if(ehString==true&&vetorComando[i]=='"'){
                 ehString=false;
             }
-            else if(i==6 && vetorComando[i]!='"'){
+            else if(i==inicioParametros && vetorComando[i]!='"'){
                 ehVariavel=true;
                 nomeVariavel+=String.valueOf(vetorComando[i]);
             }
@@ -88,8 +104,8 @@ public class ComandoPrint extends Comando<String>{
         boolean ehVariavel = false;
         boolean ehString = false;
 
-        for(int i=6; i<vetorComando.length; i++){
-            if(i==6 && vetorComando[i]!='"'){
+        for(int i=inicioParametros; i<vetorComando.length; i++){
+            if(i==inicioParametros && vetorComando[i]!='"'){
                 concatenaVariavel=true;
                 contaVariaveis++;
             }
@@ -126,7 +142,7 @@ public class ComandoPrint extends Comando<String>{
 
         contaStrings=contaStrings/2;
 
-        for (int i = 6; i < vetorComando.length; i++) {
+        for (int i = inicioParametros; i < vetorComando.length; i++) {
             if(vetorComando[i]=='"'&&ehString==false){
                 if(ehVariavel==true){
                     return false;
@@ -139,7 +155,7 @@ public class ComandoPrint extends Comando<String>{
                     return false;
                 }
             }
-            else if(i==6 && vetorComando[i]!='"'){
+            else if(i==inicioParametros && vetorComando[i]!='"'){
                 ehVariavel=true;
             }
             else if(vetorComando[i] == '+' && ehVariavel == false){
