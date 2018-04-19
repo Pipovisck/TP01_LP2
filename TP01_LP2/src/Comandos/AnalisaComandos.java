6198e5627de5f55cb9f7979f6f1b47b86bf85e54
augustonildo <augustocarvalhopp@gmail.com>
2018-04-19 09:33:06 -0300

    Mudanças no compilador e na analise de comandos

--- TP01_LP2/src/Comandos/AnalisaComandos.java
+++ TP01_LP2/src/Comandos/AnalisaComandos.java
@@ -1,6 +1,7 @@
 package Comandos;
 
 import static Constantes.Constantes.PALAVRAS_CONHECIDAS;
+import java.util.List;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -9,39 +10,47 @@ import static Constantes.Constantes.PALAVRAS_CONHECIDAS;
  */
 /**
  *
- * @author augus
+ * @author Augusto
  */
 public class AnalisaComandos {
 
     String[] palavrasReservadas;
 
     public AnalisaComandos() {
-        setPalavrasReservadas();
     }
 
-    public void setPalavrasReservadas() {
-        // seta todas as palavras reservadas da linguagem cefetiny
-    }
-
-    public Comando comparaPalavras(String comando) {
-        String[] auxComando = comando.split("");
+    public Comando comparaPalavras(List<String> comando) {
+        String[] auxComando = comando.get(0).split("");
+        String[] vetorComandos = new String[comando.size()];
+        int i = 0;
         
-        if (auxComando[0] == PALAVRAS_CONHECIDAS[12]){
-            return new ComandoPrint(comando); 
-        } else if (auxComando[0] == PALAVRAS_CONHECIDAS[13]){
-            // return new ComandoPrintln(); (?)
+        
+        if (auxComando[0] == PALAVRAS_CONHECIDAS[12] || auxComando[0] == PALAVRAS_CONHECIDAS[13]){
+            // return new ComandoPrint(comando.get(0)); 
         } else if (auxComando[0] == PALAVRAS_CONHECIDAS[14]){
-            // return new ComandoReadInt(); (?)
-        } else if (auxComando[0] == PALAVRAS_CONHECIDAS[1]){
-            // return new ComandoIf();
+            // return new ComandoReadInt(comando.get(0));
+        } else if (auxComando[0] == PALAVRAS_CONHECIDAS[1]){            
+            while(!comando.isEmpty()){
+            vetorComandos[i] = comando.remove(i);
+            i++;
+            }            
+            // return new ComandoIf(vetorComandos);
         } else if (auxComando[0] == PALAVRAS_CONHECIDAS[5]){
-            // return new ComandoWhile(Falta definir a assinatura dos parametros); 
+            while(!comando.isEmpty()){
+            vetorComandos[i] = comando.remove(i);
+            i++;
+            }  
+            // return new ComandoWhile(vetorComandos); 
         } else if (auxComando[0] == PALAVRAS_CONHECIDAS[8]){
-            // return new ComandoFor();
+            while(!comando.isEmpty()){
+            vetorComandos[i] = comando.remove(i);
+            i++;
+            }  
+            // return new ComandoFor(vetorComandos);
         } else if (auxComando [1] == ":="){
-            // return new ComandoAtribuicao(comando);
+            // return new ComandoAtribuicao(comando.get(0));
         } else if (auxComando[auxComando.length - 1] == PALAVRAS_CONHECIDAS[15]){
-            // return new ComandoEnd();(?)
+            // return new ComandoEnd();
         }    
 
         return null;
