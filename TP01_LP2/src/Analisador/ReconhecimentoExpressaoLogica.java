a0cfea83bea669d3b93c830a2f14813c15247345
Matheus <matheusqs10000@hotmail.com.br>
2018-04-19 12:34:48 -0300

    Quase terminei a reconhecimento de expressoes logicas

--- TP01_LP2/src/Analisador/ReconhecimentoExpressaoLogica.java
+++ TP01_LP2/src/Analisador/ReconhecimentoExpressaoLogica.java
@@ -5,16 +5,23 @@
  */
 package Analisador;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Stack;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  *
  * @author mathe
+ * @param <E>
  */
-public class ReconhecimentoExpressaoLogica {
+public class ReconhecimentoExpressaoLogica<E> {
+
+    private final String[] precedencia = {"||", "&&"};
 
     private final Stack<String> operandos;
     private final Stack<String> operadores;
+    private final ConcurrentLinkedQueue<Boolean> resultados;
     private final CalculosLogicos calculos;
 
     private enum UltimoCaracter {
@@ -26,6 +33,7 @@ public class ReconhecimentoExpressaoLogica {
     public ReconhecimentoExpressaoLogica() {
         operandos = new Stack<>();
         operadores = new Stack<>();
+        resultados = new ConcurrentLinkedQueue();
         calculos = new CalculosLogicos();
         ultimoCaracter = UltimoCaracter.operando;
     }
@@ -35,21 +43,63 @@ public class ReconhecimentoExpressaoLogica {
         for (String caracter : caracteresExpressao) {
             switch (caracter) {
                 case "=":
+                    operadores.add(caracter);
                     ultimoCaracter = UltimoCaracter.operador;
                     break;
                 case ">":
+                    operadores.add(caracter);
                     ultimoCaracter = UltimoCaracter.operador;
                     break;
                 case "<":
+                    operadores.add(caracter);
                     ultimoCaracter = UltimoCaracter.operador;
                     break;
                 case "|":
+                    if (ultimoCaracter == UltimoCaracter.operador) {
+                        this.desenpilhaResolveAnterior();
+                        operadores.set(operadores.size() - 1, operadores.lastElement() + caracter);
+                    } else {
+                        operadores.add(caracter);
+                    }
+                    ultimoCaracter = UltimoCaracter.operador;
+                    break;
+                case "&":
+                    if (ultimoCaracter == UltimoCaracter.operador) {
+                        this.desenpilhaResolveAnterior();
+                        operadores.set(operadores.size() - 1, operadores.lastElement() + caracter);
+                    } else {
+                        operadores.add(caracter);
+                    }
                     ultimoCaracter = UltimoCaracter.operador;
                     break;
                 default:
+                    if (ultimoCaracter == UltimoCaracter.operando && !operandos.isEmpty()) {
+                        operandos.set(operandos.size() - 1, operandos.lastElement() + caracter);
+                    } else {
+                        operandos.add(caracter);
+                    }
                     ultimoCaracter = UltimoCaracter.operando;
             }
         }
-        return false;
+        return this.resolverExpressao();
     }
+
+    public void desenpilhaResolveAnterior() {
+        String numero2 = operandos.pop();
+        String numero1 = operandos.pop();
+        resultados.add(calculos.calcularExpressaoLogicaBinaria(numero1, numero2, operadores.pop()));
+    }
+
+    public boolean resolverExpressao() {
+        this.desenpilhaResolveAnterior();
+        while (!operadores.empty()) {
+            Boolean resultado = calculos.calcularExpressaoLogicaBinaria(resultados.remove(), resultados.remove(), operadores.pop());
+            if (operadores.empty()) {
+                resultados.add(resultado);
+            }
+            operandos.add(resultado.toString());
+        }
+        return Boolean.parseBoolean(operandos.lastElement());
+    }
+
 }
--- TP01_LP2/src/Compilador/Aplicacao.java
+++ TP01_LP2/src/Compilador/Aplicacao.java
@@ -1,5 +1,6 @@
 package Compilador;
 
+import Analisador.ReconhecimentoExpressaoLogica;
 import Excecoes.ExcecaoArquivoVazio;
 
 /**
@@ -9,9 +10,12 @@ import Excecoes.ExcecaoArquivoVazio;
 public class Aplicacao {
 
     public static void main(String[] args) throws ExcecaoArquivoVazio {
-        Compilador compilador = new Compilador();
-        compilador.rodarPrograma();
-
+//        Compilador compilador = new Compilador();
+//        compilador.rodarPrograma();
+        ReconhecimentoExpressaoLogica x;
+        x = new ReconhecimentoExpressaoLogica();
+        String expressao = "5>4 || 3<2";
+        System.out.println(x.calcularExpressao(expressao));
 //O programa esta imprimindo para teste duas vezes. 
 //Para parar com esse teste, comentar as linhas 47, 48, 49, 50 e 51 da classe LeituraArquivo
 //Para parar com esse teste, comentar as linhas 32, 33 e 34 da classe AnalisaPrograma
