bfb1b2e2a000489578d4061127cee1d6b5d4820f
Pipovisck <goncalves.pipo2123@gmail.com>
2018-04-19 12:44:53 -0300

    Continuação da classe ComandoFor

--- TP01_LP2/src/Comandos/ComandoFor.java
+++ TP01_LP2/src/Comandos/ComandoFor.java
@@ -2,6 +2,7 @@ package Comandos;
 
 import Analisador.ReconhecimentoExpressoes;
 import Memoria.Memoria;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -12,10 +13,15 @@ public class ComandoFor extends Comando{
     private List<Comando> listaComandos;
     private String[] vetorLinhas;
     private Memoria memoria;
+    private boolean to;
+    private boolean downto;
+    private int fimAtribuicao;
 
-    public ComandoFor(List<Comando> listaComandos, String[] vetorLinhas) {
-        this.listaComandos = listaComandos;
+    public ComandoFor(String[] vetorLinhas) {
         this.vetorLinhas = vetorLinhas;
+        this.listaComandos = pegaListaComandos();
+        this.downto = false;
+        this.to = false;
     }
 
     @Override
@@ -26,6 +32,7 @@ public class ComandoFor extends Comando{
         String[] partesAtribuicao;
         String nomeVar;
         int valorVar;
+        String condicaoParada;
         char[] linha1 = vetorLinhas[0].toCharArray();
         String linhaSemEspaco = tiraEspaco(linha1);
         
@@ -43,7 +50,32 @@ public class ComandoFor extends Comando{
         }else{
             memoria.setVariavel(nomeVar, valorVar);
         }
-
+        
+        condicaoParada = pegaCondicaoParada(linha1);
+        
+        //Cria o laço de repetição
+        int iterador = Integer.parseInt(memoria.getVariavel(nomeVar).toString());
+        
+//        if (this.downto == true) {
+//            while(condicaoParada == true){
+//                listaComandos.forEach((comando) -> {
+//                    comando.executar(this.memoria);
+//                });
+//                
+//                iterador--;
+//                memoria.setVariavel(nomeVar, iterador);
+//            }
+//            
+//        }else if(this.to == true){
+//            while(condicaoParada == true){
+//                listaComandos.forEach((comando) -> {
+//                    comando.executar(this.memoria);
+//                });
+//                
+//                iterador++;
+//                memoria.setVariavel(nomeVar, iterador);
+//            }
+//        }
 
         return this.memoria;
     }
@@ -51,7 +83,8 @@ public class ComandoFor extends Comando{
     @Override
     public boolean verificarSintaxe() {
         String comando = new String();
-        String atribuicao;
+        String atribuicao = new String();
+        String condicaoParada = new String();
         String[] partesAtribuicao = new String[2];
         char[] linha1 = vetorLinhas[0].toCharArray();
         String linhaSemEspaco = tiraEspaco(linha1);
@@ -73,9 +106,25 @@ public class ComandoFor extends Comando{
             return false;
         }
         
-        return false;
+        //Parte que confere se existe condicao de parada
+        condicaoParada = pegaCondicaoParada(linha1);
+        
+        if (condicaoParada == null) {
+            return false;
+        }
+        
+        
+        return true;
     }
     
+    private List<Comando> pegaListaComandos(){
+        List listaComand = new LinkedList();
+        for (int i = 1; i < vetorLinhas.length - 1; i++) {
+            listaComand.add(vetorLinhas[i]);
+        }
+        
+        return listaComand;
+    }
     
     private String tiraEspaco(char[] linha){
         String linhaSemEspaço = new String();
@@ -91,35 +140,53 @@ public class ComandoFor extends Comando{
     
     private String pegaComandoAtribuicao(char[] linha){
         String atribuicao = new String();
-        String operador = new String();
+        String incrementador = new String();
         
         for (int i = 3; i < linha.length; i++) {
-            atribuicao += linha[i];
-            
             if (linha[i] == 'd') {
                 for (int j = i; j < i + 6; j++) {
-                    operador += linha[j];
+                    incrementador += linha[j];
                 }
                 
-                if (operador.equals("downto")) {
+                if (incrementador.equals("downto")) {
+                    this.downto = true;
+                    this.fimAtribuicao = i+7;
                     return atribuicao;
                 }else{
-                    operador = "";
+                    incrementador = "";
                 }
             }
             
             if (linha[i] == 't') {
                 for (int j = i; j < i + 2; j++) {
-                    operador += linha[j];
+                    incrementador += linha[j];
                 }
                 
-                if (operador.equals("to")) {
+                if (incrementador.equals("to")) {
+                    this.to = true;
+                    this.fimAtribuicao = i+3;
                     return atribuicao;
                 }else{
-                    operador = "";
+                    incrementador = "";
                 }
             }
+            
+            atribuicao += linha[i];
         }
+        
+        return null;
+    }
+    
+    private String pegaCondicaoParada(char[] linha){
+        String condicaoParada = new String();
+        
+        for (int i = this.fimAtribuicao; i < linha.length; i++) {
+            if (linha[i] == 'd' && linha[i + 1] == 'o' && (i + 1) == (linha.length - 1)) {
+                return condicaoParada;
+            }
+            condicaoParada += linha[i];
+        }
+        
         return null;
     }
     
