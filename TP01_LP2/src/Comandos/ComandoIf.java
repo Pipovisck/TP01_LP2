a6564c77b4850320299be1d125f6cc96caa1983d
augustonildo <augustocarvalhopp@gmail.com>
2018-04-24 23:32:15 -0300

    Esboço mais preciso da classe comandoIf

--- TP01_LP2/src/Comandos/ComandoIf.java
+++ TP01_LP2/src/Comandos/ComandoIf.java
@@ -12,6 +12,31 @@ import Memoria.Memoria;
  * @author Aluno
  */
 public class ComandoIf extends Comando {
+    
+    public ComandoIf (String[] vetorComandos){
+        int aux = 0;
+        
+        if(/* Teste de expressão lógica */ true){
+            for(int i = 0; i<vetorComandos.length;i++){
+                if(vetorComandos[i] == "endif" || vetorComandos[i] == "else"){
+                    aux = 1;
+                }
+                else if(aux == 0){
+                    // executa qualquer outro comando
+                }
+            }
+        }else{
+            for(int i = 0; i<vetorComandos.length;i++){
+                if(vetorComandos[i] == "endif"){
+                    aux = 1;
+                }
+                else if(aux == 0){
+                    // executa qualquer outro comando
+                }
+            }
+        }
+        
+    }
 
     @Override
     public Memoria executar(Memoria memoria) {
